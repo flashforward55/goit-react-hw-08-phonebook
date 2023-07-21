db3daa663f57e9b990b8c645ed73d7fe116e2523
Anton Styazhkin <rudana089@gmail.com>
2023-07-21 12:23:48 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { phonebookReducer } from "./slice";
+import * as rp from 'redux-persist'
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { phonebookUserReducer } from "./slices/userSlice";
+import { phonebookContactsReducer } from "./slices/contactsSlice";
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['token'],
+};
+
+export const persistedUserReducer = persistReducer(persistConfig, phonebookUserReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: phonebookReducer,
+    user: persistedUserReducer,
+    contacts: phonebookContactsReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [rp.FLUSH, rp.REHYDRATE, rp.PAUSE, rp.PERSIST, rp.PURGE, rp.REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = rp.persistStore(store);
