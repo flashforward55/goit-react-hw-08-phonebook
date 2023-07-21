8a7f49509e8c832fbb5ddc4be66211a4c402805c
Anton Styazhkin <rudana089@gmail.com>
2023-07-21 13:33:30 +0300

    Update Home.jsx

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { NavigateButton } from 'components/NavigateButton';
+
+export const Home = () => {
+  return (
+    <div>
+      <hi>Hello there! </hi>
+
+      <h2>
+        It seems that you have finally found a site where you can conveniently
+        store all your contacts and have access to them from any device
+      </h2>
+      <h3>Ready to work? 🏋️‍♂️</h3>
+      <NavigateButton />
+    </div>
+  );
+};
