eb638b5e74732afbe84948cfa81100534b73f48e
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 17:23:14 +0300

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,21 +1,32 @@
-import { FallingLines } from 'react-loader-spinner';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Bars } from 'react-loader-spinner';
 import styled from 'styled-components';
 
-export const Loading = () => {
-  return (
-    <StyledBox>
-      <FallingLines {...FallingLinesStyled} />
-    </StyledBox >
-  );
+export const Loading = ({ type, color, height, width }) => (
+  <LoaderWrapper>
+    <Bars type={type} color={color} height={height} width={width} />
+  </LoaderWrapper>
+);
+
+Loading.propTypes = {
+  type: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
 };
-const StyledBox = styled.div`
+
+Loading.defaultProps = {
+  type: 'TailSpin',
+  color: '#3f51b5',
+  height: '40',
+  width: '40',
+};
+
+export const LoaderWrapper = styled.div`
+  margin-bottom: 15px;
+  margin-top: 15px;
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
-
-export const FallingLinesStyled = {
-  color: "#082911",
-  width: "70",
-  visible: true,
-  ariaLabel: 'falling-lines-loading',
-};
