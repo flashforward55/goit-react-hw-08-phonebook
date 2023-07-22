db0e27c9f8194c0d84d10a6c40de37f252e95a3e
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 15:26:56 +0300

    Update component Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,72 @@
-import { NavigateButton } from "components/NavigateButton";
+/* import { NavigateButton } from 'components/NavigateButton';
 import styled from 'styled-components';
 
 export const Home = () => {
   return (
     <MainWrap>
-      <MainTitle>Hello there!</MainTitle>
-      <SecondTitle>It seems that you have finally found a site where you can conveniently store all your contacts and have access to them from any device</SecondTitle>
-      <SecondTitle>Ready to work? </SecondTitle>
+      <MainTitle>Greetings!</MainTitle>
+      <SecondTitle>
+        Welcome to your new contact management platform, where you can
+        conveniently store and access all your contacts from any device.
+      </SecondTitle>
+      <SecondTitle>Are you ready to get started?</SecondTitle>
       <NavigateButton />
-    </MainWrap>);
+    </MainWrap>
+  );
 };
 
 const SecondTitle = styled.h2`
   margin-top: 30px;
+  margin-bottom: 30px;
 `;
 
 export const MainWrap = styled.div`
-    margin-top: 126px;
-    text-align: center;
+  margin-top: 126px;
+  text-align: center;
 `;
 
 export const MainTitle = styled.h1`
-    text-align: center;
-    margin-bottom: 32px;
-    color: #fff;
+  text-align: center;
+  margin-bottom: 32px;
+  color: #fff;
+`;
+ */
+import { NavigateButton } from 'components/NavigateButton';
+import styled from 'styled-components';
+
+export const Home = () => {
+  return (
+    <MainWrap>
+      <MainTitle>Greetings!</MainTitle>
+      <SecondTitle>
+        Welcome to your new contact management platform, where you can
+        conveniently store and access all your contacts from any device.
+      </SecondTitle>
+      <SecondTitle>Are you ready to get started?</SecondTitle>
+      <NavigateButton />
+    </MainWrap>
+  );
+};
+
+const MainWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+`;
+
+const MainTitle = styled.h1`
+  font-size: 36px;
+  font-weight: bold;
+  color: #3f51b5;
+  margin-bottom: 16px;
+`;
+
+const SecondTitle = styled.h2`
+  font-size: 24px;
+  font-weight: normal;
+  color: #333;
+  text-align: center;
+  margin-bottom: 16px;
 `;
