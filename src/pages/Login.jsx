2de5bac9b90e5e7c1e442d47b0acd8fe29a445d4
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 15:32:53 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,28 @@
-import { LoginForm } from "components/LoginForm";
-import { NavigateLink } from "components/NavigateLink";
+import { LoginForm } from 'components/LoginForm';
+import { NavigateLink } from 'components/NavigateLink';
 import styled from 'styled-components';
 
 export const Login = () => {
   return (
     <StyledSection>
-      <SecondTitle>If you have an account, please login 🙋‍♂️</SecondTitle>
+      <SecondTitle>If you have an account, please login</SecondTitle>
       <LoginForm />
-      <NavigateLink way={"/register"} text={"Or maybe wanna"} />
+      <NavigateLink way={'/register'} text={'Or maybe wanna'} />
     </StyledSection>
   );
 };
 
 const StyledSection = styled.section`
-  margin-top: 7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
 `;
 
-export const SecondTitle = styled.h2`
-    text-align: center;
-    margin-bottom: 16px;
-    color: #fff;
+const SecondTitle = styled.h2`
+  font-size: 28px;
+  font-weight: normal;
+  color: #3f51b5;
+  margin-bottom: 16px;
 `;
