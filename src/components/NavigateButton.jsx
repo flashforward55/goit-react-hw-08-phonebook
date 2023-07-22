cd500fbed32a7ab2c495c225ef58d11d4c44e877
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 16:51:21 +0300

    Update NavigateButton.jsx

--- src/components/NavigateButton.jsx
+++ src/components/NavigateButton.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+/* import { useSelector } from "react-redux";
 import { selectAuth } from "redux/selector";
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 import { NavLink } from 'react-router-dom';
@@ -31,4 +31,47 @@ export const NavButton = styled(NavLink)`
 
 const StyledRocketLaunchIcon = styled(RocketLaunchIcon)`
   margin-left: 5px;
+`;
+ */
+
+import { useSelector } from 'react-redux';
+import { selectAuth } from 'redux/selector';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const NavigateButton = () => {
+  const { isLoggedIn } = useSelector(selectAuth);
+
+  return (
+    <NavButton to={isLoggedIn ? '/contacts' : '/register'}>
+      {isLoggedIn ? 'Go to Contacts' : "Let's Get Started"}
+      <StyledRocketLaunchIcon />
+    </NavButton>
+  );
+};
+
+export const NavButton = styled(NavLink)`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 24px;
+  border-radius: 8px;
+  border: 2px solid #3f51b5;
+  color: bisque;
+  background-color: #3f51b5;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    color: #5c6bc0;
+    background-color: rgba(255, 228, 196, 0.8);
+  }
+`;
+
+const StyledRocketLaunchIcon = styled(RocketLaunchIcon)`
+  margin-left: 8px;
+  font-size: 24px;
 `;
