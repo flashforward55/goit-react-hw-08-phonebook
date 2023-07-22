ec6b41b2c5af31e1eb5a50c6aa517c607d983877
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 15:30:48 +0300

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,28 @@
-import { RegisterForm } from "components/RegisterForm";
-import { NavigateLink } from "components/NavigateLink";
+import { RegisterForm } from 'components/RegisterForm';
+import { NavigateLink } from 'components/NavigateLink';
 import styled from 'styled-components';
 
 export const Register = () => {
   return (
     <StyledSection>
-      <SecondTitle>If you are new, please register 💁‍♂️</SecondTitle>
+      <SecondTitle>If you are new, please register</SecondTitle>
       <RegisterForm />
-      <NavigateLink way={"/login"} text={"Or maybe wanna"} />
+      <NavigateLink way={'/login'} text={'Or maybe wanna'} />
     </StyledSection>
   );
 };
 
 const StyledSection = styled.section`
-  margin-top: 7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
 `;
 
-export const SecondTitle = styled.h2`
-    text-align: center;
-    margin-bottom: 16px;
-    color: #fff;
+const SecondTitle = styled.h2`
+  font-size: 28px;
+  font-weight: normal;
+  color: #3f51b5;
+  margin-bottom: 16px;
 `;
