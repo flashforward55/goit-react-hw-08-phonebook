a1f23e2ca93c19b214bec8ae231ad4b7c5c15918
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 16:41:33 +0300

    Update NavigateLink.jsx

--- src/components/NavigateLink.jsx
+++ src/components/NavigateLink.jsx
@@ -1,23 +1,34 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 export const NavigateLink = ({ way, text }) => {
-  const word = way.slice(1, way.length);
+  const word = way.slice(1);
 
   return (
-    <SecondTitle>
+    <LinkContainer>
       {text} <NavToLink to={way}>{word}</NavToLink>?
-    </SecondTitle>
+    </LinkContainer>
   );
 };
 
-export const NavToLink = styled(NavLink)`
-    color: bisque;
+const LinkContainer = styled.p`
+  text-align: center;
+  margin-top: 30px;
 `;
 
-export const SecondTitle = styled.h2`
-    text-align: center;
-    margin-bottom: 16px;
-    color: #fff;
-    margin-top: 8px;
+const NavToLink = styled(NavLink)`
+  color: #3f51b5;
+  font-weight: bold;
+  text-decoration: none;
+  transition: color 0.2s ease-in-out;
+
+  &:hover {
+    color: #5c6bc0;
+  }
 `;
+
+NavigateLink.propTypes = {
+  way: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
