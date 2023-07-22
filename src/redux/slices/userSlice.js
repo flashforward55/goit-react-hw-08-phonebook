21dfe9cfbf62726cb92981ab0a9121e6f0b40971
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 14:16:35 +0300

    Update userSlice.js

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -2,43 +2,47 @@ import { createSlice } from "@reduxjs/toolkit";
 import { registerUser, logInUser, logOutUser, refreshUser } from "../operations/userOperations";
 
 const handleAuthUser = (state, action) => {
-    state.user = action.payload.user;
-    state.token = action.payload.token;
-    state.isLoggedIn = true;
+  const { user, token } = action.payload;
+  state.user = user;
+  state.token = token;
+  state.isLoggedIn = true;
 };
 
 const handleLogOutUser = (state) => {
-    state.user = { name: null, email: null };
-    state.token = null;
-    state.isLoggedIn = false;
+  state.user = { name: null, email: null };
+  state.token = null;
+  state.isLoggedIn = false;
 };
 
-const hadleRefreshUser = (state, action) => {
-    state.user = action.payload;
-    state.isLoggedIn = true;
-    state.isRefreshing = false;
+const handleRefreshUser = (state, action) => {
+  const user = action.payload;
+  state.user = user;
+  state.isLoggedIn = true;
+  state.isRefreshing = false;
 };
 
 const phonebookUserSlice = createSlice({
-    name: "user",
-    initialState: {
-        user: { name: null, email: null },
-        token: null,
-        isLoggedIn: false,
-        isRefreshing: false,
-    },
-    extraReducers: builder =>
-        builder
-            .addCase(registerUser.fulfilled, handleAuthUser)
-            .addCase(logInUser.fulfilled, handleAuthUser)
-            .addCase(logOutUser.fulfilled, handleLogOutUser)
-            .addCase(refreshUser.pending, state => {
-                state.isRefreshing = true;
-            })
-            .addCase(refreshUser.fulfilled, hadleRefreshUser)
-            .addCase(refreshUser.rejected, state => {
-                state.isRefreshing = false;
-            })
+  name: "user",
+  initialState: {
+    user: { name: null, email: null },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+  },
+  reducers: {}, // Add reducers here if needed
+  extraReducers: (builder) =>
+    builder
+      .addCase(registerUser.fulfilled, handleAuthUser)
+      .addCase(logInUser.fulfilled, handleAuthUser)
+      .addCase(logOutUser.fulfilled, handleLogOutUser)
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, handleRefreshUser)
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
+      }),
 });
 
 export const phonebookUserReducer = phonebookUserSlice.reducer;
+
