d6461a87a814aa85deb9c72e132b6204b689556c
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 13:51:07 +0300

    Fix selector.js

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,25 +1,26 @@
 import { createSelector } from "@reduxjs/toolkit";
 
 export const selectContacts = state => state.contacts.items;
-
 export const selectIsLoading = state => state.contacts.isLoading;
-
 export const selectError = state => state.contacts.error;
-
 export const selectFilter = state => state.contacts.filter;
 
-export const selectFilteredContacts = createSelector([selectContacts, selectFilter], (contacts, filter) => {
-    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-});
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
 
 export const selectUser = state => state.user.user;
-
 export const selectIsLoggedIn = state => state.user.isLoggedIn;
-
 export const selectIsRefreshing = state => state.user.isRefreshing;
 
 export const selectAuth = createSelector([selectUser, selectIsLoggedIn, selectIsRefreshing], (user, isLoggedIn, isRefreshing) => ({
-    user,
-    isLoggedIn,
-    isRefreshing,
+  user,
+  isLoggedIn,
+  isRefreshing,
 }));
