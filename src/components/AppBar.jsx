a8fea637a6fe50f738e9053e804e750fb5330eff
Anton Styazhkin <rudana089@gmail.com>
2023-07-22 17:55:44 +0300

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -25,17 +25,17 @@ const StyledHeader = styled.header`
   align-items: center;
   margin: 0 auto;
   max-width: 1280px;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
   padding: 16px;
   background-color: #3f51b5;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
 `;
 
 export const UserAuthWrap = styled.div`
   display: none;
 
   @media (min-width: 768px) {
-    display: block;
-    margin-top: 4px;
+    display: flex;
+    align-items: center;
   }
 `;
